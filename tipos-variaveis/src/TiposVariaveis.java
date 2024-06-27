5d6fda60a457afb2af2d4b00da86f17973719b9b
Felipe Ribeiro <felipe-psn-17@hotmail.com>
2024-06-27 18:55:24 -0300

    adicionando variaveis e seus tipos testados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-    byte idade = 123;
-    short ano = 2021;
-    int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
-    long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
-    float pi = 3.14F;
-    double salario = 1275.33;
+    String meuNome = "Felipe Ribeiro";
+    double salarioMinimo = 2500;
+    byte idade = 24;
+	short ano = 2024;
+	int cep = 27936490; // se começar com zero, talvez tenha que ser outro tipo
+	long rg = 22805727; // se começar com zero, talvez tenha que ser outro tipo
+	float pi = 3.14F;
+	double salario = 1700;
+    final double VALOR_DE_PI = 3.14; //"final" não permite alterações posteriores;
+    System.out.println(meuNome);
+    System.out.println(salarioMinimo);
+    System.out.println(idade);
+    System.out.println(ano);
+    System.out.println(cep);
+    System.out.println(rg);
+    System.out.println(salario);
+    System.out.println(pi); // alteravels
+    System.out.println(VALOR_DE_PI); // nao alteravel
+
+
+
 }
 
 }
